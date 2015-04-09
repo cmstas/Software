6bc45fe17e760b91fc1cae98e108b24421dfd70d
fgolf <frank.golf@gmail.com>
2015-04-09 16:34:53 -0700

    update to work with ROOT6

--- MiniFWLite/MiniFWLiteLinkDef.h
+++ MiniFWLite/MiniFWLiteLinkDef.h
@@ -6,6 +6,9 @@
 #pragma link off all functions;
 #pragma link off all globals;
 
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
 #pragma includepath "$ROOTSYS/include"
 #include "Math/LorentzVector.h"
 #pragma link C++ class ROOT::Math::PxPyPzE4D<float>+;
@@ -20,6 +23,7 @@
 #pragma link C++ class vector<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> > >+;
 
 // for CMS2 classes
+#include "TString.h"
 #pragma link C++ class edm::Wrapper<TString>+;
 #pragma link C++ class edm::Wrapper<double>+;
 #pragma link C++ class edm::Wrapper<float>+;
@@ -47,47 +51,6 @@
 #pragma link C++ class edm::Wrapper<vector<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,allocator<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > >,allocator<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,allocator<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > > > > >+;
 #pragma link C++ class edm::Wrapper<vector<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag>,allocator<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag> > >,allocator<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag>,allocator<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag> > > > > >+;
 #pragma link C++ class edm::Wrapper<vector<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag>,allocator<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> > >,allocator<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag>,allocator<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> > > > > >+;
-
-namespace edm 
-{
-typedef Wrapper<vector<TString,allocator<TString> > > Wrapper<vector<TString> >;
-typedef Wrapper<vector<double,allocator<double> > > Wrapper<vector<double> >;
-typedef Wrapper<vector<float,allocator<float> > > Wrapper<vector<float> >;
-typedef Wrapper<vector<int,allocator<int> > > Wrapper<vector<int> >;
-typedef Wrapper<vector<unsigned int,allocator<unsigned int> > > Wrapper<vector<unsigned int> >;
-typedef Wrapper<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >,allocator<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > > > > Wrapper<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > > >;
-typedef Wrapper<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,allocator<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > > > Wrapper<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > >;
-typedef Wrapper<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag>,allocator<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag> > > > Wrapper<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag> > >;
-typedef Wrapper<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag>,allocator<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> > > > Wrapper<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> > >;
-typedef Wrapper<vector<vector<TString,allocator<TString> >,allocator<vector<TString,allocator<TString> > > > > Wrapper<vector<vector<TString> > >;
-typedef Wrapper<vector<vector<double,allocator<double> >,allocator<vector<double,allocator<double> > > > > Wrapper<vector<vector<double> > >;
-typedef Wrapper<vector<vector<float,allocator<float> >,allocator<vector<float,allocator<float> > > > > Wrapper<vector<vector<float> > >;
-typedef Wrapper<vector<vector<int,allocator<int> >,allocator<vector<int,allocator<int> > > > > Wrapper<vector<vector<int> > >;
-typedef Wrapper<vector<vector<unsigned int,allocator<unsigned int> >,allocator<vector<unsigned int,allocator<unsigned int> > > > > Wrapper<vector<vector<unsigned int> > >;
-typedef Wrapper<vector<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >,allocator<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > > >,allocator<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >,allocator<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > > > > > > Wrapper<vector<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > > > >;
-typedef Wrapper<vector<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,allocator<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > >,allocator<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,allocator<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > > > > > Wrapper<vector<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > > >;
-typedef Wrapper<vector<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag>,allocator<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag> > >,allocator<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag>,allocator<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag> > > > > > Wrapper<vector<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag> > > >;
-typedef Wrapper<vector<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag>,allocator<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> > >,allocator<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag>,allocator<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> > > > > > Wrapper<vector<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> > > >;
-};
-
-#pragma link C++ typedef edm::Wrapper<vector<TString> >;
-#pragma link C++ typedef edm::Wrapper<vector<double> >;
-#pragma link C++ typedef edm::Wrapper<vector<float> >;
-#pragma link C++ typedef edm::Wrapper<vector<int> >;
-#pragma link C++ typedef edm::Wrapper<vector<unsigned int> >;
-#pragma link C++ typedef edm::Wrapper<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > > >;
-#pragma link C++ typedef edm::Wrapper<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > >;
-#pragma link C++ typedef edm::Wrapper<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag> > >;
-#pragma link C++ typedef edm::Wrapper<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> > >;
-#pragma link C++ typedef edm::Wrapper<vector<vector<TString> > >;
-#pragma link C++ typedef edm::Wrapper<vector<vector<double> > >;
-#pragma link C++ typedef edm::Wrapper<vector<vector<float> > >;
-#pragma link C++ typedef edm::Wrapper<vector<vector<int> > >;
-#pragma link C++ typedef edm::Wrapper<vector<vector<unsigned int> > >;
-#pragma link C++ typedef edm::Wrapper<vector<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > > > >;
-#pragma link C++ typedef edm::Wrapper<vector<vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > > >;
-#pragma link C++ typedef edm::Wrapper<vector<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>,ROOT::Math::DefaultCoordinateSystemTag> > > >;
-#pragma link C++ typedef edm::Wrapper<vector<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> > > >;
 
 // for DataFormats/Common and DataFormats/Provenance
 #pragma link C++ class edm::BranchChildren+;
@@ -151,8 +114,9 @@ typedef Wrapper<vector<vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian
 #pragma link C++ class edm::IndexIntoFile::RunOrLumiEntry+;
 #pragma link C++ class edm::IndexIntoFile::Transients+;
 #pragma link C++ class edm::Wrapper<TBits>+;
+
 using std::pair;
-using std::set;
+#include <set>
 #pragma link C++ class set<edm::BranchID>+;
 #pragma link C++ class pair<edm::BranchID, set<edm::BranchID> >+;
 #pragma link C++ class map<edm::BranchID, set<edm::BranchID> >+;
--- MiniFWLite/TaSBanner.cc
+++ MiniFWLite/TaSBanner.cc
@@ -1,28 +1,29 @@
 #include "Rtypes.h"
 #include "Riostream.h"
 #include "TSystem.h"
+#include <iostream>
 
 const char* VTAG="02-YY-ZZ";
 
 Int_t doBanner() {
     if (! gSystem->Getenv("WHOISTAS")) {
-        cout << endl
+        std::cout << std::endl
              << "\033[1mMiniFWLite v" << VTAG
-                                 << " -- A FORTRAN++ data analysis framework developed by TaS\033[0m" << endl;
+                                 << " -- A FORTRAN++ data analysis framework developed by TaS\033[0m" << std::endl;
     } else {
-        cout << endl
+        std::cout << std::endl
              << "\033[1mMiniFWLite v" << VTAG
-                                 << " -- A FORTRAN++ data analysis framework developed by TaS,  a.k.a. Turf and Surf,"<< endl
-             << "                        a.k.a. West Coast Mafia,  a.k.a. Victory Squad,  a.k.a. The Beaver Stompers."<< endl
-             << "                        Mad props to FG3 & Punizzle for carrying the load on the nTuple code. Shout-"<< endl
-             << "                        outs to  Jmu West Psi keeping  it real  at a bird's-eye  view,  The Bristol" << endl
-             << "                        Pistol sans-daft-izzle, and  Warren G Hardly needs characterization.  Holla" << endl
-             << "                        at my FNAL boyz Oli Oli Oxendatenführer, Ingo's the Lingo Can Sing-Oh!, and" << endl
-             << "                        Burkett Tracks-charged-particles-for-miles. Much respect to the rest of the" << endl
-             << "                        UCSD, UCSB and FNAL crewz.  If your name is not explicitly mentioned, it is" << endl
-             << "                        not for lack of love, it is due solely to lack of imagination and indolence.\033[0m" << endl;
+                                 << " -- A FORTRAN++ data analysis framework developed by TaS,  a.k.a. Turf and Surf,"<< std::endl
+             << "                        a.k.a. West Coast Mafia,  a.k.a. Victory Squad,  a.k.a. The Beaver Stompers."<< std::endl
+             << "                        Mad props to FG3 & Punizzle for carrying the load on the nTuple code. Shout-"<< std::endl
+             << "                        outs to  Jmu West Psi keeping  it real  at a bird's-eye  view,  The Bristol" << std::endl
+             << "                        Pistol sans-daft-izzle, and  Warren G Hardly needs characterization.  Holla" << std::endl
+             << "                        at my FNAL boyz Oli Oli Oxendatenführer, Ingo's the Lingo Can Sing-Oh!, and" << std::endl
+             << "                        Burkett Tracks-charged-particles-for-miles. Much respect to the rest of the" << std::endl
+             << "                        UCSD, UCSB and FNAL crewz.  If your name is not explicitly mentioned, it is" << std::endl
+             << "                        not for lack of love, it is due solely to lack of imagination and indolence.\033[0m" << std::endl;
     }
-        cout << "                        http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/UserCode/JRibnik/CMS2" << endl << endl;
+        std::cout << "                        http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/UserCode/JRibnik/CMS2" << std::endl << std::endl;
 
     return 0 ;
 }
