1a2233ae044c28a1bdfe8caa1659e9f71059ec90
Christopher A. George <george@physics.ucsb.edu>
2015-05-29 15:56:19 -0700

    development

--- alexBeamer/README.md
+++ alexBeamer/README.md
@@ -8,7 +8,7 @@ This package generates beamer presentations with minimal effort.
 ###Instructions:
   1. Declare with "pres name"
   2. Declare "color" with "name.KeyColor('blue')".  You can use use black, red, green, blue, cyan, magenta, yellow, and darkgreen.  Others can be added, follow the procedure for darkgreen.
-  3. "test.Underline('name')" to get your name bolded and underlined.  Right now only supported for Alex; add your name in the same way.
+  3. "name.Underline('name')" to get your name bolded and underlined.  Right now only supported for Alex; add your name in the same way.
   4.  Make title slide with name.TitleSlide("Presentation Name").
   5.  All other sides should be defined with name.NewSlide(), then the body, then name.FinishSlide()
 
--- alexBeamer/test.C
+++ alexBeamer/test.C
@@ -10,10 +10,41 @@ void test(){
   //Second slide
   myTest.NewSlide();
   myTest.Title("This is my Awesome Second Slide with a Really Long Name.");
+  myTest.AllText(R"(
+  \begin{itemize} 
+  \item This is orthogonal to our search region, which has SIP3D $<$ 4 
+  \item Derived only on events with: 
+  \begin{itemize}
+  \item 2+ jets
+  \item HT $>$ 80
+  \item veto if MET $<$ 30 \&\& HT $<$ 500
+  \item Low mass and Z/g* veto
+  \item One lepton truth-matched to prompt (from W), the other truth-matched to fake (from b, c, light)
+  \begin{itemize}
+  \item truth-matching is slightly different than we normally use
+  \end{itemize}
+  \end{itemize}
+  \item Lepton selections for denominator and numerator here are same as elsewhere except for: 
+  \begin{itemize}
+  \item SIP3D inverted
+  \item no dxy cut
+  \item Denominator iso: 
+  \begin{itemize}
+  \item miniIso $<$ 0.4 (FO1 definition)
+  \item miniIso $<$ 0.4 \&\& (ptRel $>$ 7.0 (6.7) $||$ jet/lep $<$ 1/0.7 (0.68) + miniIso) (FO4 definition)
+  \end{itemize}
+  \end{itemize}
+  \end{itemize} 
+  )", 0);
+
   myTest.FinishSlide();
 
   //Third slide
   myTest.NewSlide();
+  myTest.Title("This is my Awesome Third Slide with a Really Long Name.");
+  myTest.DoubleCompare("result_elec.pdf", "result_muon.pdf"); 
+  myTest.Text(R"(Error bars represent errors \underline{in MC}, with binomial uncertainty formula.)", 0);
+  myTest.FreeText(0.92, 0.8, "FO1"); 
   myTest.FinishSlide();
 
 }
