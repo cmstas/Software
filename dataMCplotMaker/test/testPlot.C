f5dfa6d354706c739241a12a0843205d7765a5d1
Nick Amin <amin.nj@gmail.com>
2015-08-11 14:23:29 -0700

    add quicktest files

---
+++ dataMCplotMaker/test/testPlot.C
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include "TROOT.h"
+#include "../dataMCplotMaker.h"
+
+using namespace std;
+
+void testPlot() {
+    std::vector<std::string> titles = { "first", "second", "third"};
+    std::vector<TH1F*> h1D_vec;
+    TH1F* data = new TH1F("data","",50,0,50);
+    TH1F *null = new TH1F("null","",1,0,1);
+    TH1F *h1D_one = new TH1F("h1D_one","",50,-5,5);
+    TH1F *h1D_two = new TH1F("h1D_two","",50,-5,5);
+    TH1F *h1D_three = new TH1F("h1D_three","",50,-5,5);
+    h1D_vec.push_back(h1D_one);
+    h1D_vec.push_back(h1D_two);
+    h1D_vec.push_back(h1D_three);
+
+    h1D_one->FillRandom("gaus",300);
+    h1D_two->FillRandom("gaus",100);
+    h1D_three->FillRandom("gaus",1000);
+    data->FillRandom("gaus",1000);
+
+    std::string common = " --xAxisOverride [GeV] --legendTaller -0.05  --outOfFrame --legendUp 0.1 --percentageInBox ";
+    dataMCplotMaker(data,h1D_vec,titles,"this is a title","",common+" --outputName h1D_vec.pdf");
+    // dataMCplotMaker(null,h1D_vec,titles,"this is a title","",common+" --outputName "+out+"h1D_vec.pdf");
+}
