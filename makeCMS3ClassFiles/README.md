ed5ee7e700ab5c496fd90b6ff9338ce403a547f1
sicenw <sicheng@physics.ucsb.edu>
2016-05-22 21:57:35 -0700

    Update some whitespace and stuff

--- makeCMS3ClassFiles/README.md
+++ makeCMS3ClassFiles/README.md
@@ -4,29 +4,31 @@ makeCMS3ClassFiles
 This function quickly makes class files -- for CMS3 or anything else -- as well as the skeleton for a looper.
 
 ###To download this:
+``` bash
 git clone https://github.com/cmstas/software.git
+```
 
 ###To run:
-  1.  root
-  2.  .L makeCMS3ClassFiles.C++
-  3.  makeCMS3ClassFiles("file you want to run on.root", "tree within that file", "class name", "namespace name", "object name")
+  1.  Start ROOT `root`
+  2.  In ROOT command line load `.L makeCMS3ClassFiles.C++`
+  3.  `makeCMS3ClassFiles("file you want to run on.root", "tree within that file", "class name", "namespace name", "object name")`
 
-N.B. The branchNamesFile variable is hardcoded to null, and paranoid is an optional sixth argument that defaults to false.  If you want to change the branchNamesFile, you should do so in the file directly.  If you have no idea what I'm talking about, don't worry about it.  
+N.B. The branchNamesFile variable is hardcoded to null, and paranoid is an optional sixth argument that defaults to false.  If you want to change the branchNamesFile, you should do so in the file directly.  If you have no idea what I'm talking about, don't worry about it.
 
 ###For a skeleton looper:
-  Doing the above will also make ScanChain.C and doAll.C, which is a skeleton looper.  
+  Doing the above will also make ScanChain.C and doAll.C, which is a skeleton looper.
 
 ###For New Students:
-  1.  Download this and run it.  For the first argument, use any sample in /hadoop/cms/store/group/snt/phys14 (ideally pick the most recent SMS sample you can find in that directory, but any sample really should work for most purposes).  This will produce CMS3.cc CMS3.h doAll.C and ScanChain.C.  
+  1.  Download this and run it.  For the first argument, use any sample in /hadoop/cms/store/group/snt/phys14 (ideally pick the most recent SMS sample you can find in that directory, but any sample really should work for most purposes).  This will produce CMS3.cc CMS3.h doAll.C and ScanChain.C.
   2.  Test this by running "root doAll.C".  If you have troubles, your ROOT is probably installed incorrectly, ask smart people for help.
-  3.  Now you write your code in looper.C and run it in doAll.C.  You can use this ScanChain to fill plots, make babies, whatever you want.  
+  3.  Now you write your code in looper.C and run it in doAll.C.  You can use this ScanChain to fill plots, make babies, whatever you want.
 
-###Troubleshooting: 
+###Troubleshooting:
   - The most common symptom is getting all 0s back.  In this case:
     - Make sure your class files are generated on the exact file you're running over.  In particular, if branches have been added or removed, it can easily cause problems
     - If you're running on CMS3.cc/CMS3.h, make sure you update CORE and rerun.  The .so file in CORE depends on the CMS3 class files, and you don't want a mismatch.
     - If you're making private class files, make sure not to call them CMS3.cc/CMS3.h.  This will cause conflicts and undefind behavior.
-    - If it complains about a ROOT version mismatch, recompile CORE from your current terminal.  That mismatch has been known to lead to this behavior.  
+    - If it complains about a ROOT version mismatch, recompile CORE from your current terminal.  That mismatch has been known to lead to this behavior.
   - If it says that a branch does not exist, you're not doing the Init and/or GetEntry properly in your looper.
 
 E-mail Alex with bug reports or feature requests, george AT physics DOT ucsb
--- makeCMS3ClassFiles/makeCMS3ClassFiles.C
+++ makeCMS3ClassFiles/makeCMS3ClassFiles.C
@@ -113,8 +113,10 @@ void makeHeaderFile(TFile *f, const string& treeName, bool paranoid, const strin
   
   
     headerf << "// -*- C++ -*-" << endl;
+    headerf << "// This is a header file generated with the command:" << endl;
+    headerf << "// makeCMS3ClassFiles(\"" << f->GetPath() << "\b\b\", \"" << treeName << "\", \"" << Classname << "\", \"" << nameSpace << "\", \"" << objName << "\")" << endl << endl;
     headerf << "#ifndef " << Classname << "_H" << endl;
-    headerf << "#define " << Classname << "_H" << endl;
+    headerf << "#define " << Classname << "_H" << endl << endl;
     headerf << "#include \"Math/LorentzVector.h\"" << endl;
     headerf << "#include \"Math/Point3D.h\"" << endl;
     headerf << "#include \"TMath.h\"" << endl;
@@ -126,8 +128,6 @@ void makeHeaderFile(TFile *f, const string& treeName, bool paranoid, const strin
     headerf << "#include <vector> " << endl;
     headerf << "#include <unistd.h> " << endl;
     headerf << "typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > LorentzVector;" << endl << endl;
-    headerf << "// Generated with the command" << endl;
-    headerf << "// makeCMS3ClassFiles(\"" << f->GetPath() << "\", \"" << treeName << "\", \"" << Classname << "\", \"" << nameSpace << "\", \"" << objName << "\")" << endl << endl;
     if (paranoid)
         headerf << "#define PARANOIA" << endl << endl;
     headerf << "using namespace std; " << endl;
@@ -269,21 +269,21 @@ void makeHeaderFile(TFile *f, const string& treeName, bool paranoid, const strin
                 }
             } else {
                 if(title.EndsWith("/i"))
-                    headerf << "\tunsigned int" << "\t" << aliasname << "_;" << endl;
+                    headerf << "\tunsigned int" << aliasname << "_;" << endl;
                 if(title.EndsWith("/l"))
-                    headerf << "\tunsigned long long" << "\t" << aliasname << "_;" << endl;
+                    headerf << "\tunsigned long long" << aliasname << "_;" << endl;
                 if(title.EndsWith("/F"))
-                    headerf << "\tfloat" << "\t" << aliasname << "_;" << endl;
+                    headerf << "\tfloat    " << aliasname << "_;" << endl;
                 if(title.EndsWith("/I"))
-                    headerf << "\tint" << "\t" << aliasname << "_;" << endl;
+                    headerf << "\tint      " << aliasname << "_;" << endl;
                 if(title.EndsWith("/O"))
-                    headerf << "\tbool" << "\t" << aliasname << "_;" << endl;
+                    headerf << "\tbool     " << aliasname << "_;" << endl;
                 if(title.EndsWith("/D"))
-                    headerf << "\tdouble" << "\t" << aliasname << "_;" << endl;
+                    headerf << "\tdouble   " << aliasname << "_;" << endl;
             }
         }
         headerf << "\tTBranch *" << Form("%s_branch",aliasname.Data()) << ";" << endl;
-        headerf << "\tbool " << Form("%s_isLoaded",aliasname.Data()) << ";" << endl;
+        headerf << "\tbool     " << Form("%s_isLoaded",aliasname.Data()) << ";" << endl;
     }
     headerf << "public: " << endl;
     headerf << "void Init(TTree *tree);" << endl;
@@ -333,9 +333,9 @@ void makeHeaderFile(TFile *f, const string& treeName, bool paranoid, const strin
                 if(title.EndsWith("/I"))
                     headerf << "\tconst int &" << aliasname << "();" << endl;
                 if(title.EndsWith("/O"))
-                    headerf << "\tconst bool &" << "\t" << aliasname << "();" << endl;
+                    headerf << "\tconst bool &" << aliasname << "();" << endl;
                 if(title.EndsWith("/D"))
-                    headerf << "\tconst double &" << "\t" << aliasname << "();" << endl;
+                    headerf << "\tconst double &" << aliasname << "();" << endl;
             }
         }
     } // end of accessor header
@@ -693,9 +693,9 @@ void makeCCFile(TFile *f, const string& Classname, const string& nameSpace, cons
                 if(title.EndsWith("/I"))
                     implf << "\tconst int &" << funcname << "()" << endl;
                 if(title.EndsWith("/O"))
-                    implf << "\tconst bool &" << "\t" << funcname << "()" << endl;
+                    implf << "\tconst bool &" << funcname << "()" << endl;
                 if(title.EndsWith("/D"))
-                    implf << "\tconst double &" << "\t" << funcname << "()" << endl;
+                    implf << "\tconst double &" << funcname << "()" << endl;
             }
         }
         aliasname = aliasarray->At(i)->GetName();
