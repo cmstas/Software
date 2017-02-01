f7d4cafc4f20f5505632a183799824c2ae3ee6b5
dsklein <dsklein@physics.ucsd.edu>
2017-02-01 15:29:36 -0800

    Fix memory leak from histogram that's not deleted

--- dataMCplotMaker/dataMCplotMaker.cc
+++ dataMCplotMaker/dataMCplotMaker.cc
@@ -1248,6 +1248,7 @@ void dataMCplotMaker(TH1F* Data_in, std::vector <std::pair <TH1F*, TH1F*> > Back
   if (nDivisions != -1 && nDivisions < 0) stack->GetXaxis()->SetNdivisions(nDivisions, kFALSE);
 
   //-----------Second pad: data/MC yields---------------
+  TH1F* background_syst_ratio = 0;
   if ((!noData || compareMultiple) && !noRatioPlot){
     if (compareMultiple) Data = (TH1F*)Signals[0]->Clone();
     if (compareMultiple && Signals.size() > 1) for (unsigned int i = 1; i < Signals.size(); i++) Data->Add(Signals[i]); 
@@ -1262,7 +1263,6 @@ void dataMCplotMaker(TH1F* Data_in, std::vector <std::pair <TH1F*, TH1F*> > Back
     err_hist->GetYaxis()->SetTitle("Data/MC");
     err_hist->GetYaxis()->SetTitleSize(0.08);
     err_hist->GetYaxis()->SetTitleOffset(1.8);
-    TH1F* background_syst_ratio = 0; 
     if (Background_systs.size() > 0) background_syst_ratio = (TH1F*)background_syst->Clone();
     for (int ib = 1; ib < err_hist->GetNbinsX()+1; ib++){
       float MC_value = 0;
@@ -1344,7 +1344,7 @@ void dataMCplotMaker(TH1F* Data_in, std::vector <std::pair <TH1F*, TH1F*> > Back
     if (Backgrounds_pair[i].second != 0) delete Backgrounds_pair[i].second;
   }
   delete background_syst;
-
+  if( background_syst_ratio != 0 ) delete background_syst_ratio;
 }
 
 //Overload function for case of no stat errors
